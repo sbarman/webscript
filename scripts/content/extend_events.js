8d02e651767b28857fc7410e8ea8d249e36dcda7
sbarman <sbarman@L3CWZ5T.(none)>
2012-09-26 13:53:03 -0700

    Cleaned up global namespace

--- scripts/content/extend_events.js
+++ scripts/content/extend_events.js
@@ -3,48 +3,52 @@
 
 'use strict';
 
-var changeRecord = function(eventData, eventMessage) {
-  var element = eventData.srcElement;
-  var nodeName = element.nodeName.toLowerCase();
-  if (nodeName == "select") {
-    eventMessage.value = element.value;
-    //throw "not sure what needs to be done";
-  } else if (nodeName == "textarea") {
-    eventMessage.value = element.value;
-  } else if (nodeName == "input") {
-    var elementType = element.type.toLowerCase();
-    if (elementType == "checkbox" || elementType == "radio") {
-      eventMessage.checked = element.checked;
-    } else if (elementType == "password" || elementType == "search" ||
-               elementType == "text" || elementType == "file" ||
-               elementType == "range") {
-      eventMessage.value = element.value
+var extendEvents = {};
+
+(function() {
+  function changeRecord(eventData, eventMessage) {
+    var element = eventData.srcElement;
+    var nodeName = element.nodeName.toLowerCase();
+    if (nodeName == "select") {
+      eventMessage.value = element.value;
+      //throw "not sure what needs to be done";
+    } else if (nodeName == "textarea") {
+      eventMessage.value = element.value;
+    } else if (nodeName == "input") {
+      var elementType = element.type.toLowerCase();
+      if (elementType == "checkbox" || elementType == "radio") {
+        eventMessage.checked = element.checked;
+      } else if (elementType == "password" || elementType == "search" ||
+                 elementType == "text" || elementType == "file" ||
+                 elementType == "range") {
+        eventMessage.value = element.value
+      }
     }
   }
-};
-
-var changeReplay = function(element, eventMessage) {
-  var nodeName = element.nodeName.toLowerCase();
-  if (nodeName == "select") {
-    element.value = eventMessage.value;
-    //throw "not sure what needs to be done";
-  } else if (nodeName == "textarea") {
-    element.value = eventMessage.value;
-  } else if (nodeName == "input") {
-    var elementType = element.type.toLowerCase();
-    if (elementType == "checkbox" || elementType == "radio") {
-      element.checked = eventMessage.checked;
-    } else if (elementType == "password" || elementType == "search" ||
-               elementType == "text" || elementType == "file" ||
-               elementType == "range") {
+  
+  function changeReplay(element, eventMessage) {
+    var nodeName = element.nodeName.toLowerCase();
+    if (nodeName == "select") {
       element.value = eventMessage.value;
+      //throw "not sure what needs to be done";
+    } else if (nodeName == "textarea") {
+      element.value = eventMessage.value;
+    } else if (nodeName == "input") {
+      var elementType = element.type.toLowerCase();
+      if (elementType == "checkbox" || elementType == "radio") {
+        element.checked = eventMessage.checked;
+      } else if (elementType == "password" || elementType == "search" ||
+                 elementType == "text" || elementType == "file" ||
+                 elementType == "range") {
+        element.value = eventMessage.value;
+      }
     }
   }
-};
-
-var extendEvents = {
-  "change": {
-    record: changeRecord,
-    replay: changeReplay
-  }
-}
+  
+  extendEvents = {
+    "change": {
+      record: changeRecord,
+      replay: changeReplay
+    }
+  };
+})();
