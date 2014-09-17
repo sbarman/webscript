5ff5004b79435b29442d85571cf643af7a7bd5c6
Shaon Barman <shaon.barman@gmail.com>
2014-09-17 16:46:44 -0700

    cleanup

---
+++ src/main/scripts/common/scope.js
@@ -0,0 +1,9 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+
+'use strict';
+
+/* Create an object containing all functions that should be accessible outside
+ * of the project */
+
+var Ringer = {};
